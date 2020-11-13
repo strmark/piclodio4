c71241671b61a9c0758047b7928f295600797544
Mark Striekwold <strmark@hotmail.com>
2020-11-13 23:10:00 +0100

    Updated angular

--- front/src/app/alarm-clock/alarm-clock.component.spec.ts
+++ front/src/app/alarm-clock/alarm-clock.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AlarmClockComponent } from './alarm-clock.component';
 
@@ -6,7 +6,7 @@ describe('AlarmClockComponent', () => {
   let component: AlarmClockComponent;
   let fixture: ComponentFixture<AlarmClockComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AlarmClockComponent ]
     })
