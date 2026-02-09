1dc6fd1a5726b26e203a291b4ec37f7768f7256a
Mark Striekwold <strmark@hotmail.com>
2026-02-09 23:13:21 +0100

    restore assert

--- back/src/test/java/nl/oradev/piclodio/PiclodioApplicationTests.java
+++ back/src/test/java/nl/oradev/piclodio/PiclodioApplicationTests.java
@@ -3,12 +3,14 @@ package nl.oradev.piclodio;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class PiclodioApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
-
